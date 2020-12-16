ab65643010e1aa6930ebd4596b728e46006aa12f
KolRobOsk <robertoskarkolke@gmail.com>
2020-12-17 00:20:57 +0100

    Lab 1 i 2 (zmiana hasla tylko po zalogowaniu ze strony glownej

---
+++ Lab1_2_Kolke/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='szczeg_post'),
+    path('post/new', views.post_new, name='post_new'),
+    path('logout/', views.logout_view, name='logout'),
+    path('password/', views.password_view, name='password'),
+]
---
+++ Lab1_2_Kolke/blog/views.py
@@ -0,0 +1,49 @@
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
+from .models import Post
+from django.contrib import auth
+from django.contrib.auth import logout, update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posty': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/szczeg_post.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('szczeg_post', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/edycja_post.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+def password_view(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('../logout/')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'blog/change_password.html', {
+        'form': form
+    })
