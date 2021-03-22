a8b5a0cb6e64916238ac6f5e12c3b97107050ab1
KolRobOsk <robertoskarkolke@gmail.com>
2021-03-22 13:44:39 +0100

    Drobne poprawki

--- redis-python/thumbnailer/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-  path('', views.HomeView.as_view(), name='home'),
-  path('task/<str:task_id>/', views.TaskView.as_view(), name='task'),
-]
